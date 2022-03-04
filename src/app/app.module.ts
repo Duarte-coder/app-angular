884921e2d2a952ba10ea699d37109c2b570ca0a7
Duarte-coder <gdbiel773@gmail.com>
2022-03-04 11:55:21 -0300

    reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonListComponent
   ],
   imports: [
     BrowserModule
